f925a1efd2ddabdec090e7e17bd84a39dc1245a6
marvin <eu@thiago.info>
2021-03-13 09:29:01 -0300

    fix loop

--- pkg/tv/tv.go
+++ pkg/tv/tv.go
@@ -16,30 +16,40 @@ func Canal(s string) {
 func Tvi() {
 	var cmd *exec.Cmd
 	tv := config.GetTvIp()
+
 	i := 1
 	for i <= 6 {
 		cmd = exec.Command("samsungctl", "--host", tv, "--id", "42", "KEY_VOLUP")
+
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
+
 		err := cmd.Run()
 		if err != nil {
 			log.Println(err)
 		}
+
+		i += 1
 	}
 }
 
 func Tvd() {
 	var cmd *exec.Cmd
 	tv := config.GetTvIp()
+
 	i := 1
 	for i <= 6 {
 		cmd = exec.Command("samsungctl", "--host", tv, "--id", "42", "KEY_VOLDOWN")
+
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
+
 		err := cmd.Run()
 		if err != nil {
 			log.Println(err)
 		}
+
+		i += 1
 	}
 }
 
@@ -48,25 +58,31 @@ func closeAll() {
 	tv := config.GetTvIp()
 
 	cmd = exec.Command("samsungctl", "--host", tv, "--id", "42", "KEY_DTV")
+
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
 	err := cmd.Run()
 	if err != nil {
 		log.Println(err)
 	}
-
 }
 
 func canalgo(s string) {
 	var cmd *exec.Cmd
+	ch := "KEY_" + s
+
 	tv := config.GetTvIp()
 
-	cmd = exec.Command("samsungctl", "--host", tv, "--id", "42", s)
+	closeAll()
+
+	cmd = exec.Command("samsungctl", "--host", tv, "--id", "42", ch)
+
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
 	err := cmd.Run()
 	if err != nil {
 		log.Println(err)
 	}
-
 }
